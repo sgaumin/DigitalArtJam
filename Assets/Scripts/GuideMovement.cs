cca6957f324681b5770e573e142dbeb2864159e0
Sebastien Gaumin <sgaumin@gmail.com>
2019-03-02 11:10:59 +0100

    NavMesh

---
+++ Assets/Scripts/GuideMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuideMovement : MonoBehaviour
+{
+    [SerializeField] private Transform[] destinations; // Use Queue instead ?
+    [SerializeField] private float timeToChange;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            Transform nextPosition = destinations[Random.Range(0, destinations.Length)];
+
+            agent.SetDestination(nextPosition.position);
+
+            yield return new WaitForSeconds(timeToChange);
+        }
+    }
+}
