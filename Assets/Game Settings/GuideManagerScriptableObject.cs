9de20c668c7b10e83f5277d39cb5632bfaceffac
Baptiste Perraud <baptiste.perraud@orange.fr>
2019-03-03 11:06:33 +0100

    Fix alarm system + add scriptable objects for settings

---
+++ Assets/Game Settings/GuideManagerScriptableObject.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/GuideManagerScriptableObject", order = 1)]
+public class GuideManagerScriptableObject : ScriptableObject
+{
+    public float sightDistance;
+}
---
+++ Assets/Game Settings/VisitorManagerScriptableObject.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/VisitorManagerScriptableObject", order = 1)]
+public class VisitorManagerScriptableObject : ScriptableObject
+{
+    public float sightDistance;
+}
